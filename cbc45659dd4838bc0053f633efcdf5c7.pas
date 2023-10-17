b01e7e5edb9ca8dadaebcddbcb3a5429501eb35b
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-10-17 10:26:26 +0300

    Add files via upload

---
+++ cbc45659dd4838bc0053f633efcdf5c7.pas
@@ -0,0 +1,16 @@
+﻿program lab1;
+var x, zn :real;
+const e = 2.71828; 
+begin
+  writeln ('Введите значение функции x');
+  readln (x);
+  if x<-6 
+  then zn:=(x/95)*(1/x)
+  else
+    if (-6<=x) and (x<-2)
+    then zn:=exp(x*ln(e))*exp(x*ln(e))-cos(2*x)
+  else
+    if -2<=x 
+    then zn:=cos(x);
+  writeln ('Значение функции равно ', zn);
+end.
